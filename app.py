021e08312cff1fac19d1c97c8a3bd386f8c3b91a
Tech <suresh.sakthivel@indiqube.com>
2019-02-13 12:34:04 +0530

    make it better

--- app.py
+++ app.py
@@ -1,34 +1,29 @@
-import os
-from flask import Flask
-from flask_restful import Api
+from flask import Flask, request
+from flask_restful import Resource,Api,reqparse
 from flask_jwt import JWT
-
-
-
-from security import authenticate, identity
+from resources.item import Item,ItemList
 from resources.user import UserRegister
-from resources.item import Item, ItemList
+
+from security import authenticate,identity
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABSE_URL','sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['PROPAGATE_EXCEPTIONS'] = True
-app.secret_key = 'jose'
+app.secret_key = 'jose'    
 api = Api(app)
-
+jwt = JWT(app,authenticate,identity)
 
 @app.before_first_request
 def create_tables():
     db.create_all()
 
-
-jwt = JWT(app, authenticate, identity)  # /auth
-
-api.add_resource(Item, '/item/<string:name>')
-api.add_resource(ItemList, '/items')
-api.add_resource(UserRegister, '/register')
+api.add_resource(Item,'/item/<string:name>')
+api.add_resource(ItemList,'/items')
+api.add_resource(UserRegister,'/signup')
 
 if __name__ == '__main__':
-    from db import db
+    from flask_sqlalchemy import  SQLAlchemy
+    db = SQLAlchemy()
     db.init_app(app)
-    app.run(port=5000, debug=True)
+    app.run(port=5000,debug=True)
