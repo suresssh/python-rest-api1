e8e875ddc7116adaf2a38aa1347edc72f9dc44db
Tech <suresh.sakthivel@indiqube.com>
2019-02-13 12:28:49 +0530

    make it better

--- app.py
+++ app.py
@@ -1,28 +1,34 @@
-from flask import Flask, request
-from flask_restful import Resource,Api,reqparse
+import os
+from flask import Flask
+from flask_restful import Api
 from flask_jwt import JWT
-from resources.item import Item,ItemList
+
+
+
+from security import authenticate, identity
 from resources.user import UserRegister
-
-from security import authenticate,identity
-
+from resources.item import Item, ItemList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABSE_URL','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'jose'    
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.secret_key = 'jose'
 api = Api(app)
 
-# @app.before_first_request
-# def create_tables():
-#     db.create_all()
-    
-jwt = JWT(app,authenticate,identity)
-api.add_resource(Item,'/item/<string:name>')
-api.add_resource(ItemList,'/items')
-api.add_resource(UserRegister,'/signup')
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+jwt = JWT(app, authenticate, identity)  # /auth
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
-    app.run(port=5000,debug=True)
+    app.run(port=5000, debug=True)
