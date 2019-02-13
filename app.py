80cb9ac86a0986f5a2cfbee6c38aad767f718491
Tech <suresh.sakthivel@indiqube.com>
2019-02-13 12:23:15 +0530

    make it better

--- app.py
+++ app.py
@@ -13,8 +13,10 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'    
 api = Api(app)
 
-
-
+@app.before_first_request
+def create_tables():
+    db.create_all()
+    
 jwt = JWT(app,authenticate,identity)
 api.add_resource(Item,'/item/<string:name>')
 api.add_resource(ItemList,'/items')
@@ -22,8 +24,5 @@ api.add_resource(UserRegister,'/signup')
 
 if __name__ == '__main__':
     from db import db
-    @app.before_first_request
-        def create_tables():
-    db.create_all()
     db.init_app(app)
     app.run(port=5000,debug=True)
