d33135dc3076c6ebcef75e0454af9393e3fac7a1
Tech <suresh.sakthivel@indiqube.com>
2019-02-13 13:03:03 +0530

    make it better

--- app.py
+++ app.py
@@ -12,15 +12,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'    
 api = Api(app)
+
 jwt = JWT(app,authenticate,identity)
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 api.add_resource(Item,'/item/<string:name>')
 api.add_resource(ItemList,'/items')
 api.add_resource(UserRegister,'/signup')
 
 if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
     app.run(port=5000,debug=True)
